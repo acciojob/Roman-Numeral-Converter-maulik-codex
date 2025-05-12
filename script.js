f44e09f32629d25c577d6dcef2ee7a5aea09ecca
maulik-codex <maulikchhansiya@gmail.com>
2025-05-12 14:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,32 +2,30 @@ function convertToRoman(num) {
     if (num <= 0 || num > 100000) return "Invalid Input";
 
     const romanSymbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
+       0: ['M', 1000],
+       1: ['CM', 900],
+       2: ['D', 500],
+       3: ['CD', 400],
+       4: ['C', 100],
+       5: ['XC', 90],
+       6: ['L', 50],
+       7: ['XL', 40],
+       8: ['X', 10],
+       9: ['IX', 9],
+      10: ['V', 5],
+      11: ['IV', 4],
+      12: ['I', 1]
     ];
-
-    let result = '';
-
-    for (const [symbol, value] of romanSymbols) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
-    }
-
-    return result;
+	let ans = "";
+	while (num !== 0){
+		for(let i in romanMap){
+			let [symbol,value] = romanMap[i];
+			if (value <= num){
+				ans += symbol;
+				num -= value;
+				break;
+			}
+		}
+	}
+    return ans;
 }
-console.log(convertToRoman(14));     // Output: XIV
-
-
