d74058381422e645790c0cec9f7b68e35541a53a
maulik-codex <maulikchhansiya@gmail.com>
2025-05-13 14:05:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 function convertToRoman(num) {
-    if (num <= 0 || num > 100000) return "Invalid Input";
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
+const values = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    const romanSymbols = [
-       0: ['M', 1000],
-       1: ['CM', 900],
-       2: ['D', 500],
-       3: ['CD', 400],
-       4: ['C', 100],
-       5: ['XC', 90],
-       6: ['L', 50],
-       7: ['XL', 40],
-       8: ['X', 10],
-       9: ['IX', 9],
-      10: ['V', 5],
-      11: ['IV', 4],
-      12: ['I', 1]
-    ];
-	let ans = "";
-	while (num !== 0){
-		for(let i in romanMap){
-			let [symbol,value] = romanMap[i];
-			if (value <= num){
-				ans += symbol;
-				num -= value;
-				break;
-			}
-		}
-	}
-    return ans;
+  let result = '';
+
+  for (let [roman, value] of values) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
+    }
+  }
+
+  return result;
 }
+console.log(convertToRoman(36));
+module.exports = convertToRoman
